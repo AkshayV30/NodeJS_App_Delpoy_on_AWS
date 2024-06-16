daf6fb5c0b0f5c822a0c906b9b2608815fe7273a
akshay <akshayatwork.v30@gmail.com>
2024-06-16 17:15:50 +0530

    restart

--- terraform/modules/lb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "application_load_balancer_name" {
-  type = string
-}
-variable "target_group_name" {
-  type = string
-}
-variable "container_port" {
-  type = number
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "docker_ecr_uri" {
-  description = "The Docker ECR URL"
-  type        = string
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(string)
-}
